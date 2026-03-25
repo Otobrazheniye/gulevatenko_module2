808415e3d2ce12ea41c10b4446706173454a3e25
Andrii Gulevatenko <otobrazheniye@gmail.com>
2026-03-25 02:31:26 +0100

    All tasks complited

---
+++ test_game.py
@@ -0,0 +1,242 @@
+from game.score import *
+from game.models import *
+from game.game import *
+
+import pytest
+
+
+# <!-------- PLAYER ---------!>
+@pytest.mark.parametrize(
+    "name, lives, score",
+    [
+        ("Oleg", 3, 0),
+    ]
+)
+def test_create_player(name, lives, score):
+    tplayer = Player(name)
+
+    assert tplayer.name == name
+    assert tplayer.lives == lives
+    assert tplayer.score == score
+
+
+@pytest.mark.parametrize("score",[0])
+def test_add_score(score):
+    tplayer = Player("test_Andrii")
+    player_add_score(tplayer)
+    assert tplayer.score == score + 1 
+
+@pytest.mark.parametrize("lives",[3])
+def test_decrease_lives(lives):
+    tplayer = Player("test_Andrii")
+    tplayer.lives = lives
+    player_decrease_lives(tplayer)
+    assert tplayer.lives == lives - 1
+
+@pytest.mark.parametrize("expected_exception, lives",[(GameOver,1)])
+def test_decrease_game_over(expected_exception,lives):
+    tplayer = Player("test_Andrii")
+    tplayer.lives = lives
+    with pytest.raises(expected_exception):
+        player_decrease_lives(tplayer)        
+
+
+# <!-------- ENEMY ---------!>
+
+@pytest.mark.parametrize(
+    "mode, level, expected_lives",
+    [
+        (Normal(), 1, 1),
+        (Normal(), 3, 3),
+        (Hard(), 1, 3),
+        (Hard(), 2, 4),
+    ]
+)
+def test_enemy_lives(mode, level, expected_lives):
+    tenemy = Enemy(mode, level)
+
+    assert tenemy.lives == expected_lives
+
+
+@pytest.mark.parametrize(
+    "player_history, expected_attacks",
+    [
+        (1, {settings.PAPER, settings.STONE}),
+        (2, {settings.STONE, settings.SCISSORS}),
+        (3, {settings.PAPER, settings.SCISSORS}),
+    ]
+)
+def test_select_attack(player_history, expected_attacks):
+    tenemy = Enemy(Hard(), 1)
+    tenemy.player_history = player_history
+
+    enemy_attack = enemy_select_attack(tenemy)
+
+    assert enemy_attack in expected_attacks
+
+@pytest.mark.parametrize("lives,expected_exception",[(1,EnemyDown)])
+def test_enemy_decrease_lives(lives,expected_exception):
+    tenemy = Enemy(Hard(),1)
+    tenemy.lives = lives
+    with pytest.raises(expected_exception):
+        enemy_decrease_lives(tenemy)
+
+# <!-------- Game ---------!>
+
+@pytest.mark.parametrize(
+    "tplayer_attack, tenemy_attack",[(settings.PAPER, settings.PAPER)])
+def test_fight_draw(tplayer_attack, tenemy_attack):
+    tplayer = Player("test_Andrii")
+    tenemy = Enemy(Hard(), 1)
+
+    start_player_lives = tplayer.lives
+    start_enemy_lives = tenemy.lives
+    start_score = tplayer.score
+
+    handle_round_result(tplayer, tenemy, tplayer_attack, tenemy_attack)
+
+    assert tplayer.lives == start_player_lives
+    assert tenemy.lives == start_enemy_lives
+    assert tplayer.score == start_score
+
+@pytest.mark.parametrize("tplayer_attack, tenemy_attack",[(settings.PAPER, settings.STONE)])
+def test_fight_win(tplayer_attack,tenemy_attack):
+    tplayer = Player("test_Andrii")
+    tenemy = Enemy(Hard(),1)
+
+    start_player_lives = tplayer.lives
+    start_enemy_lives = tenemy.lives
+    start_score = tplayer.score
+
+    handle_round_result(tplayer,tenemy,tplayer_attack,tenemy_attack)
+    assert tplayer.lives == start_player_lives
+    assert tenemy.lives < start_enemy_lives
+    assert tplayer.score > start_score
+
+
+@pytest.mark.parametrize("tplayer_attack, tenemy_attack",[(settings.SCISSORS, settings.STONE)])
+def test_fight_lose(tplayer_attack,tenemy_attack):
+    tplayer = Player("test_Andrii")
+    tenemy = Enemy(Hard(),1)
+
+    start_player_lives = tplayer.lives
+    start_enemy_lives = tenemy.lives
+    start_score = tplayer.score
+
+    handle_round_result(tplayer,tenemy,tplayer_attack,tenemy_attack)
+    assert tplayer.lives < start_player_lives
+    assert tenemy.lives == start_enemy_lives
+    assert tplayer.score == start_score
+
+
+
+@pytest.mark.parametrize(
+    "mode_number, expected_mode",
+    [
+        (1, models.Normal),
+        (2, models.Hard),
+    ]
+)
+def test_create_enemy(monkeypatch, mode_number, expected_mode):
+    monkeypatch.setattr(models, "choose_mode", lambda: mode_number)
+
+    enemy = create_enemy()
+
+    assert enemy.level == settings.LEVEL
+    assert isinstance(enemy.mode, expected_mode)
+
+# <!-------- PlayerRecord ---------!>
+
+def test_sort_records():
+    records = [
+        score.PlayerRecord("A", "Normal", 10),
+        score.PlayerRecord("B", "Normal", 5),
+        score.PlayerRecord("C", "Normal", 20),
+    ]
+
+    records.sort(reverse=True)
+
+    scores = [r.score for r in records]
+
+    assert scores == [20, 10, 5]
+
+def test_eq_records():
+    records = [
+        score.PlayerRecord("A", "Normal", 10),
+        score.PlayerRecord("A", "Normal", 5),
+    ]  
+
+    assert records[0] == records[1]
+
+# <!-------- GameRecord ---------!>
+
+def test_add_record():
+    game_record = GameRecord()
+    new_record = PlayerRecord("Andrii", "Hard", 6)
+
+    game_record.add_record(new_record)
+    
+    assert len(game_record.records) == 1
+    assert game_record.records[0] == new_record
+
+def test_change_record():
+    game_record = GameRecord()
+
+    new_record = PlayerRecord("Andrii","Hard",6)
+    game_record.add_record(new_record)
+
+    new_record2 = PlayerRecord("Andrii","Hard",10)
+    game_record.add_record(new_record2)
+
+    assert len(game_record.records) == 1
+    assert game_record.records[0] == new_record2
+
+def test_prepare_records_sort_and_limit(monkeypatch):
+    monkeypatch.setattr(settings, "MAX_SCORE", 2)
+
+    game_record = GameRecord()
+
+    game_record.records = [
+        PlayerRecord("A", "Normal", 10),
+        PlayerRecord("B", "Normal", 5),
+        PlayerRecord("C", "Normal", 20),
+    ]
+
+    game_record.prepare_records()
+
+    scores = [r.score for r in game_record.records]
+    assert scores == [20, 10]
+
+    assert len(game_record.records) == 2
+
+
+# <!-------- ScoreHandler: ---------!>
+
+
+def test_all_records_types():
+    total_board = ScoreHandler("dublicate.txt")
+
+    print(total_board.game_record.records)
+    assert len(total_board.game_record.records) > 0
+    for record in total_board.game_record.records:
+        assert isinstance(record.name, str)
+        assert isinstance(record.mode, str)
+        assert isinstance(record.score, int)
+
+def test_save_records(tmp_path):
+    file = tmp_path / "test.txt"
+    # создай путь внутри этой папки. tmp_path - pathlib.Path
+    # import os         file = os.path.join(tmp_path, "test.txt")
+
+    score_handler = ScoreHandler(str(file))
+    score_handler.game_record.records = [
+        PlayerRecord("Andrii", "Hard", 10),
+        PlayerRecord("Oleg", "Normal", 5),
+    ]
+    score_handler.save()
+    content = file.read_text(encoding="utf-8").splitlines()
+
+    assert content == [
+        "Andrii,Hard,10",
+        "Oleg,Normal,5",
+    ]
